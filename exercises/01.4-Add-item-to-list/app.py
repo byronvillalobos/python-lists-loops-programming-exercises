7a41999eebb24f194abb80d086cc650c6bea479a
byronvillalobos <byronvillalobos@hotmail.com>
2021-04-05 21:49:27 +0000

    comments

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,15 @@
 #Remember import random function here:
-
-my_list = [4,5,734,43,45]
-
+import random
 #The magic is here:
+
+def generate_random_items():
+    my_list = []
+    adding = random.randint(0, 1)
+
+    for i in range(0, 10(my_list)):
+        my_list.append(adding)
+        i += i
+    return my_list
+
+my_stupid_list = generate_random_items()
+print(my_stupid_list)
